bc077c319a48e35b25ea33961d2e3c7b4a9e61c0
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-04-09 00:59:23 +0300

    navbar design

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,15 +7,31 @@ const Navbar = () => {
     <div className={styles.container}>
       <div className={styles.item}>
         <div className={styles.callButton}>
-          <Image src="/img/telephone.jpeg" alt="" width="100" height="100"/>
+          <Image
+            className={styles.callButton}
+            src="/img/telephone.jpeg"
+            alt=""
+            width={50}
+            height={50}
+          />
         </div>
         <div className={styles.texts}>
           <div className={styles.text}>ORDER NOW</div>
           <div className={styles.text}>0212 121 1212</div>
         </div>
       </div>
-      <div className={styles.item}></div>
-      <div className={styles.item}></div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Homapage</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <Image src="/img/logo.png" alt="" width={70} height={70}></Image>
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>right</div>
     </div>
   );
 };
