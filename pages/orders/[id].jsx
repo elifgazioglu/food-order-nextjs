d6d88f1e5811fd703d96bcb714ee82fb153390ea
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-04-27 21:29:25 +0300

    created order wrapper

---
+++ pages/orders/[id].jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Image from "next/image";
+import styles from "@/styles/Order.module.css";
+
+const Order = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}></div>
+      <div className={styles.right}>
+        <div className={styles.wrapper}>
+          <h2 className={styles.title}>CART TOTAL</h2>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Subtotal: </b>$10.10
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Discount: </b>$0.00
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Total: </b>$10.10
+          </div>
+          <button disabled className={styles.button}>
+            PAID
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Order;
