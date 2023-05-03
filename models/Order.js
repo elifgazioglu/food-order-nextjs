f2b55ff6184537f2f0ffb177c11b22477c54987a
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-05-03 23:50:09 +0300

    update models

---
+++ models/Order.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const OrderSchema = new mongoose.Schema(
+  {
+    customer: {
+      type: String,
+      required: true,
+      maxlength: 60,
+    },
+    adress: {
+      type: String,
+      require: true,
+      maxlength: 200,
+    },
+    total: {
+      type: Number,
+      require: true,
+    },
+    status: {
+      type: Number,
+      default: 0,
+    },
+    method: {
+        type: Number,
+        required: true
+      },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Order", OrderSchema);
